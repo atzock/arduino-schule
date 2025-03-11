23fc4d9c99df9db1c866ffab5fc3d64f4a753a74
atzock <hannes.atzert@web.de>
2025-03-11 07:50:37 +0100

    initial push

--- befehlsammlung/prints.ino
+++ befehlsammlung/prints.ino
@@ -1,1 +1,6 @@
-// coming soon
+Serial.begin(9600); // print einrichten
+
+Serial.println("Hello World!"); // print mit Zeilenumbruch
+Serial.print("Hello World!"); // print ohne Zeilenumbruch
+
+Serial.print(variable, 3); //Zahl wird zum Kürzen genutzt
--- unterrichtsprogramme/ab-for-while/aufgabe4.ino
+++ unterrichtsprogramme/ab-for-while/aufgabe4.ino
@@ -1,4 +1,30 @@
 // Aufgabe 4
 // Nun soll das große 1x1 aufgesagt werden:
 
-// Programmiere kurz und elegenat und so kurz wie möglich.
+// Programmiere kurz und elegant und so kurz wie möglich.
+
+// 11x1=11; 11x2=22; ...
+// 12x1=12; 12x2=24; ...
+// ...
+// 20x1=20; 20x2=40; ...
+
+void setup() {
+    Serial.begin(9600);
+    Serial.println("Das grosse 1x1 lautet:");
+
+    for (int i = 11; i <= 20; i++) {
+        for (int j = 1; j <= 10; j++) {
+            Serial.print(i);
+            Serial.print("x");
+            Serial.print(j);
+            Serial.print("=");
+            Serial.print(i * j);
+            Serial.print("; ");
+        }
+        Serial.println();
+        delay(2000);
+    }
+}
+
+void loop() {}
+
