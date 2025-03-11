a004a0d1fe10b4e6d0c8e8d6ae845c8d7ce753f2
atzock <hannes.atzert@web.de>
2025-03-11 08:15:28 +0100

    more befehlssammlung

--- befehlsammlung/prints.ino
+++ befehlsammlung/prints.ino
@@ -4,3 +4,4 @@ Serial.println("Hello World!"); // print mit Zeilenumbruch
 Serial.print("Hello World!"); // print ohne Zeilenumbruch
 
 Serial.print(variable, 3); //Zahl wird zum Kürzen genutzt
+
--- befehlsammlung/schleifen.ino
+++ befehlsammlung/schleifen.ino
@@ -1,1 +1,9 @@
-// coming soon
+for (int i = 0; i < 10; i++) {
+    Serial.println(i);
+}
+
+int j = 0;
+while (j < 10) {
+    Serial.println(j);
+    j++;
+}
