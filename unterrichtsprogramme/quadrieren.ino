dc51141082756de1c482c68be1a37316686c633c
atzock <hannes.atzert@web.de>
2025-03-11 11:44:48 +0100

    quadrieren update

---
+++ unterrichtsprogramme/quadrieren.ino
@@ -0,0 +1,27 @@
+// a
+void setup() {
+    Serial.begin(9600);
+    int zahl = 2;
+    while (zahl * zahl < 257) {
+        Serial.println(zahl * zahl);
+        zahl *= 2;
+
+    }
+}
+
+void loop() {}
+
+// b
+long ergeb = 1000001; // 1.000.001
+
+void setup() {
+    Serial.begin(9600);
+    long zahl = 2;
+    while (zahl * zahl < ergeb) {
+        Serial.println(zahl * zahl);
+        zahl *= 2;
+
+    }
+}
+
+void loop() {}
