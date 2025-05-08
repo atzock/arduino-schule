80440ff17f80a1ad9def79295a6f4a8a36176b11
atzock <hannes.atzert@web.de>
2025-05-08 11:33:01 +0200

    added ampel script to speedrun.ino

--- unterrichtsprogramme/speedrun/speedrun.ino
+++ unterrichtsprogramme/speedrun/speedrun.ino
@@ -21,3 +21,57 @@ void loop() {
   previousTasterStatus = currentTasterStatus;
   delay(50);
 }
+
+// Ampel:
+const int buttonPin = 2;
+const int redPin = 8;
+const int greenPin = 10;
+
+// Stats
+const int STATE_IDLE = 0;
+const int STATE_WAITING = 1;
+const int STATE_GREEN = 2;
+
+int currentState = STATE_IDLE;
+unsigned long stateStartTime = 0;
+
+void setup() {
+  pinMode(buttonPin, INPUT_PULLUP); // Taster an GND
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+
+  digitalWrite(redPin, HIGH);  // Start mit Rot
+  digitalWrite(greenPin, LOW);
+}
+
+void loop() {
+  unsigned long now = millis();
+
+  if (currentState == STATE_IDLE) {
+    digitalWrite(showPin, LOW);
+    if (digitalRead(buttonPin) == LOW) {
+      currentState = STATE_WAITING;
+      stateStartTime = now;
+    }
+  }
+
+  else if (currentState == STATE_WAITING) {
+    digitalWrite(showPin, HIGH);
+    if (now - stateStartTime >= 20000) {  // 20 Sek. Warten
+      digitalWrite(redPin, LOW);
+      digitalWrite(greenPin, HIGH);
+      currentState = STATE_GREEN;
+      stateStartTime = now;
+    }
+  }
+
+  else if (currentState == STATE_GREEN) {
+    digitalWrite(showPin, LOW);
+    if (now - stateStartTime >= 30000) {  // 30 Sek. Grün
+      digitalWrite(greenPin, LOW);
+      digitalWrite(redPin, HIGH);
+      currentState = STATE_IDLE;
+    }
+  }
+}
+
